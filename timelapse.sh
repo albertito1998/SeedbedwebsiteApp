3922679c2b3bb7b660b6d1accfe7a8190fed6815
Alberto Gómez <57259704+albertito1998@users.noreply.github.com>
2020-08-17 14:20:06 +0200

    Delete timelapse.sh

--- timelapse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/pi/webcam/photos
-ls *.jpg > stills.txt 
-mencoder /home/pi/webcam/timelapse -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=640:480 -o timelapse.avi -mf type=jpeg:fps=1 mf://@stills.
-
-
-#CODE TO ADD IN CRONTAB
-#* * 15 * * /home/pi/webcam/timelapse.sh 2>&1
